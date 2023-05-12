940c2b8a263fb59813bba3d08d9b1257d5846758
Manuel Avelar <me@mavelar.com>
2023-05-12 13:06:02 -0600

    work in progress

---
+++ Block/Adminhtml/Order/View.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace DUna\Payments\Block\Adminhtml\Order;
+
+use Magento\Sales\Block\Adminhtml\Order\View as OrderView;
+
+/**
+ * Adminhtml sales order view.
+ * @api
+ * @since 100.2.2
+ */
+class View extends OrderView
+{
+
+}
