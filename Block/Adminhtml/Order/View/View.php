88ad6d5ed288ce70cce48b50ea911f4c56501b71
Manuel Avelar <me@mavelar.com>
2023-05-15 08:38:33 -0600

    base setup

---
+++ Block/Adminhtml/Order/View/View.php
@@ -0,0 +1,10 @@
+<?php
+namespace DUna\Payments\Block\Adminhtml\Order\View;
+class View extends \Magento\Backend\Block\Template
+{
+    public function myFunction()
+    {
+        //your code
+        return "Customers' Instruction";
+    }
+}
