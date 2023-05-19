d0dc4d2e6ed2ebfb418af1a9c3388f973ab538c2
Engelbertg J Bracho R <jesusramirez@destiny.ws>
2023-05-19 18:09:02 -0400

    [JS-PICKUP-BBB] Add EventListener

--- view/frontend/web/js/checkout-widget.js
+++ view/frontend/web/js/checkout-widget.js
@@ -106,55 +106,57 @@ define(components, function ($, Component, ko, Url, DeunaCDL, DunaCheckout) {
     });
 });
 
-var shippingMethodForm = document.getElementById('block-shipping-top');
+window.addEventListener('load', (event) => {
+    var shippingMethodForm = document.getElementById('block-shipping-top');
 
-if (shippingMethodForm != null) {
-    var radioElements = shippingMethodForm.querySelectorAll('input[type="radio"]');
-    var checkoutButton = document.getElementById('duna-checkout').querySelector('button');
+    if (shippingMethodForm != null) {
+        var radioElements = shippingMethodForm.querySelectorAll('input[type="radio"]');
+        var checkoutButton = document.getElementById('duna-checkout').querySelector('button');
 
-    function updateCheckoutButton() {
-        for (var i = 0; i < radioElements.length; i++) {
-            if (radioElements[i].checked) {
-                checkoutButton.disabled = false;
-                return;
-            }
-        }
-        checkoutButton.disabled = true;
-    }
-
-    updateCheckoutButton()
-
-    for (var i = 0; i < radioElements.length; i++) {
-        radioElements[i].addEventListener('change', updateCheckoutButton);
-    }
-
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === XMLHttpRequest.DONE) {
-            if (xhr.status === 200) {
-                var response = xhr.responseText;
-                var div = document.createElement('div');
-                div.innerHTML = response;
-
-                var tdElements = div.querySelectorAll('td');
-
-                if (tdElements.length > 0) {
-                } else {
-                    var csMethodBopisBopis = document.getElementById('cs_method_bopis_bopis');
-                    var parentElement = csMethodBopisBopis.parentNode;
-                    parentElement.setAttribute('hidden', 'true');
+        function updateCheckoutButton() {
+            for (var i = 0; i < radioElements.length; i++) {
+                if (radioElements[i].checked) {
+                    checkoutButton.disabled = false;
+                    return;
                 }
-            } else {
-                console.log('La petición GET ha finalizado con un error.');
             }
+            checkoutButton.disabled = true;
         }
-    };
 
-    var currentURL = window.location.href;
-    var urlObject = new URL(currentURL);
-    var domain = urlObject.origin;
+        updateCheckoutButton()
 
-    xhr.open('GET', domain + '/storepickup/stores/index/?_=' + Date.now());
-    xhr.send();
+        for (var i = 0; i < radioElements.length; i++) {
+            radioElements[i].addEventListener('change', updateCheckoutButton);
+        }
 
-} 
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === XMLHttpRequest.DONE) {
+                if (xhr.status === 200) {
+                    var response = xhr.responseText;
+                    var div = document.createElement('div');
+                    div.innerHTML = response;
+
+                    var tdElements = div.querySelectorAll('td');
+
+                    if (tdElements.length > 0) {
+                    } else {
+                        var csMethodBopisBopis = document.getElementById('cs_method_bopis_bopis');
+                        var parentElement = csMethodBopisBopis.parentNode;
+                        parentElement.setAttribute('hidden', 'true');
+                    }
+                } else {
+                    console.log('La petición GET ha finalizado con un error.');
+                }
+            }
+        };
+
+        var currentURL = window.location.href;
+        var urlObject = new URL(currentURL);
+        var domain = urlObject.origin;
+
+        xhr.open('GET', domain + '/storepickup/stores/index/?_=' + Date.now());
+        xhr.send();
+
+    } 
+});
