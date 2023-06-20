4edaa1463a5e551c1eb12fc2a0d3a1032665c9f4
Engelbertg J Bracho R <3ng3l64@gmail.com>
2023-06-20 15:17:42 -0400

    [PLAT-875]

--- Observer/CheckoutErrorHandler.php
+++ Observer/CheckoutErrorHandler.php
@@ -13,7 +13,7 @@ class CheckoutErrorHandler implements ObserverInterface
         $request = $observer->getEvent()->getRequest();
         $response = $observer->getEvent()->getResponse();
         
-        if ($response->getContent() === 'Please correct the country code: %1.' || $response->getContent() === 'Invalid Country Code') {
+        if ($response->getContent() === 'Invalid Country Code') {
             $errorMessage = __('Verifica tu información de entrega y código postal.');
             $response->setContent($errorMessage);
             $response->setHttpResponseCode(400);
